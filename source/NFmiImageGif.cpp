122cedc47b02079ff02bee745eec5263646d110c
mheiskan <mheiskan>
2004-02-27 10:58:54 +0000

    imagine osaa nyt lukea giffejä

--- source/NFmiImageGif.cpp
+++ source/NFmiImageGif.cpp
@@ -2,12 +2,8 @@
 //
 // NFmiImage addendum - GIF reading and writing
 //
-// History:
-//
-// 26.10.2001 Mika Heiskanen
-//
-//	Implemented writing based on ppmtogif.c and coders/gif.c
-//
+// Code originally from the pbmplus package
+
 // ======================================================================
 
 #include "NFmiImage.h"
@@ -23,16 +19,570 @@ using namespace std;
 namespace Imagine
 {
 
-  // Maximum number of colors allowed in a GIF file
-  
-  const int kFmiMaxGifColors = 256;
+  //! Maximum number of colors allowed in a GIF file
+  const int MaxColors = 256;
+
+  //! The bit used to indicate interlacing
+  const unsigned char Interlace = 0x40;
+
+  //! The bit used to indicate a local color map
+  const unsigned char LocalColorMap = 0x80;
+
+  //! Maximum number of LZW code bits
+  const int MaxLzwBits = 12;
+
+  static bool zeroblock = false;
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Test if bit is set
+   */
+  // ----------------------------------------------------------------------
+
+  template <typename T>
+  inline
+  bool bitset(T theByte, T theBit)
+  {
+	return ((theByte & theBit) == theBit);
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Convert chars to unsigned int
+   */
+  // ----------------------------------------------------------------------
+
+  inline
+  unsigned int LM_to_uint(unsigned char a, unsigned char b)
+  {
+	return ((b<<8) | a);
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Reader function
+   */
+  // ----------------------------------------------------------------------
+
+  inline
+  bool readok(FILE * theInput,
+			  unsigned char * theBuffer,
+			  int theLength)
+  {
+	return (fread(theBuffer,theLength,1,theInput) != 0);
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief GIF information holder
+   */
+  // ----------------------------------------------------------------------
+
+  struct GifScreen
+  {
+	unsigned int	width;
+	unsigned int	height;
+	unsigned char	colormap[3][MaxColors];
+	unsigned int	bitpixel;
+	unsigned int	colorresolution;
+	unsigned int	background;
+	unsigned int	aspectratio;
+
+	// Gif89
+
+	int	transparent;
+	int delaytime;
+	int inputflag;
+	int disposal;
+
+	GifScreen()
+	  : width(0)
+	  , height(0)
+	  , bitpixel(0)
+	  , colorresolution(0)
+	  , background(0)
+	  , aspectratio(0)
+	  , transparent(-1)
+	  , delaytime(-1)
+	  , inputflag(-1)
+	  , disposal(0)
+	{ }
+
+  };
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Read GIF color map from the input file
+   */
+  // ----------------------------------------------------------------------
+
+  void read_colormap(FILE * theInput,
+					 int theBits,
+					 unsigned char theColorMap[3][MaxColors])
+  {
+	unsigned char rgb[3];
+	for(int i=0; i<theBits; ++i)
+	  {
+		if(!readok(theInput,rgb,3))
+		  throw runtime_error("Bad GIF colormap");
+		theColorMap[0][i] = rgb[0];
+		theColorMap[1][i] = rgb[1];
+		theColorMap[2][i] = rgb[2];
+	  }
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Read an extension data block
+   */
+  // ----------------------------------------------------------------------
+
+  int getdatablock(FILE * theInput,
+				   unsigned char * theBuffer)
+  {
+	int count;
+	if( (count=fgetc(theInput)) == EOF)
+	  throw runtime_error("Block size missing from GIF stream");
+
+	zeroblock = (count == 0);
+
+	if(count != 0)
+	  {
+		if(!readok(theInput,theBuffer,count))
+		  throw runtime_error("Error reading GIF data block");
+	  }
+
+	return count;
+	
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Read GIF extension block
+   */
+  // ----------------------------------------------------------------------
+
+  void read_extension(FILE * theInput,
+					  int theLabel,
+					  GifScreen & theScreen)
+  {
+	unsigned char buffer[256];
+
+	switch(theLabel)
+	  {
+	  case 0x01:	// plain text extension
+	  case 0xff:	// application extension
+	  case 0xfe:	// comment extension
+		break;
+	  case 0xf9:	// graphic control extension
+		{
+		  getdatablock(theInput,buffer);
+		  theScreen.disposal    = (buffer[0] >> 2) & 0x7;
+		  theScreen.inputflag   = (buffer[0] >> 1) & 0x1;
+		  theScreen.delaytime   = LM_to_uint(buffer[1],buffer[2]);
+		  if ((buffer[0] & 0x1) != 0)
+			theScreen.transparent = buffer[3];
+		  break;
+		}
+	  default:
+		throw runtime_error("Unknown extension block in GIF");
+	  }
+
+	// skip remaining info
+	while(getdatablock(theInput,buffer) != 0)
+	  ;
+	
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Get a code from LZW input
+   */
+  // ----------------------------------------------------------------------
+
+  int getcode(FILE * theInput,
+			  int theCodeSize,
+			  bool theFlushFlag)
+  {
+	static unsigned char buf[280];
+	static int curbit = 0;
+	static int lastbit = 0;
+	static int lastbyte = 0;
+	static bool done = false;
+	
+	if(theFlushFlag)
+	  {
+		curbit = 0;
+		lastbit = 0;
+		done = false;
+		return 0;
+	  }
+	  
+	if(curbit + theCodeSize >= lastbit)
+	  {
+		if(done)
+		  {
+			if(curbit >= lastbit)
+			  throw runtime_error("Ran off the end of GIF LZW bits");
+			return -1;
+		  }
+
+		buf[0] = buf[lastbyte-2];
+		buf[1] = buf[lastbyte-1];
+
+		int count = getdatablock(theInput,&buf[2]);
+		
+		if(count == 0)
+		  done = true;
+
+		lastbyte = 2 + count;
+		curbit = (curbit - lastbit) + 16;
+		lastbit = (2+count)*8;
+	  }
+
+	int ret = 0;
+	for(int i=curbit, j=0; j<theCodeSize; ++i, ++j)
+	  ret |= ((buf[ i / 8 ] & (1 << (i % 8))) != 0) << j;
+
+	curbit += theCodeSize;
+
+	return ret;
+	  
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Read next LZW byte
+   */
+  // ----------------------------------------------------------------------
+
+  int lzwreadbyte(FILE * theInput,
+				  bool theFlag,
+				  int theInputCodeSize)
+  {
+	static int set_code_size = 0;
+	static int code_size = 0;
+	static int clear_code = 0;
+	static int end_code = 0;
+	static int max_code_size = 0;
+	static int max_code = 0;
+	static int firstcode = 0;
+	static int oldcode = 0;
+	static bool fresh = false;
+	static int table[2][1 << MaxLzwBits];
+	static int stack[(1<<(MaxLzwBits))*2], *sp;
+
+	int i;
+
+	if(theFlag)
+	  {
+		set_code_size = theInputCodeSize;
+		code_size = set_code_size + 1;
+		clear_code = 1 << set_code_size;
+		end_code = clear_code + 1;
+		max_code_size = 2*clear_code;
+		max_code = clear_code + 2;
+		
+		getcode(theInput,0,true);
+		
+		fresh = true;
+		
+		for(i=0; i<clear_code; ++i)
+		  {
+			table[0][i] = 0;
+			table[1][i] = i;
+		  }
+		for( ; i<(1<<MaxLzwBits); ++i)
+		  table[0][i] = table[1][0] = 0;	// BUG???
+
+		sp = stack;
+		return 0;
+	  }
+
+	else if(fresh)
+	  {
+		fresh = false;
+		do
+		  firstcode = oldcode = getcode(theInput,code_size,false);
+		while (firstcode == clear_code);
+		return firstcode;
+	  }
+
+	if(sp>stack)
+	  return *--sp;
+
+	int code;
+	while((code=getcode(theInput,code_size,false)) >= 0)
+	  {
+		if(code == clear_code)
+		  {
+			for(i=0; i<clear_code; ++i)
+			  {
+				table[0][i] = 0;
+				table[1][i] = i;
+			  }
+			for( ; i<(1<<MaxLzwBits); ++i)
+			  table[0][i] = table[1][i] = 0;
+			code_size = set_code_size + 1;
+			max_code_size = 2*clear_code;
+			max_code = clear_code + 2;
+			sp = stack;
+			firstcode = oldcode = getcode(theInput,code_size,false);
+			return firstcode;
+		  }
+		else if(code == end_code)
+		  {
+			if(zeroblock)
+			  return -2;
+
+			unsigned char buf[260];
+			int count;
+			while((count = getdatablock(theInput,buf)) > 0)
+			  ;
+
+			// This is apparently common:
+			// if(count != 0)
+			// throw runtime_error("Missing EOD in GIF stream");
+			
+			return -2;
+
+		  }
+
+		int incode = code;
+
+		if(code >= max_code)
+		  {
+			*sp++ = firstcode;
+			code = oldcode;
+		  }
+		while(code >= clear_code)
+		  {
+			*sp++ = table[1][code];
+			if(code == table[0][code])
+			  throw runtime_error("Circular table entry in GIF");
+			code = table[0][code];
+		  }
+
+		*sp++ = firstcode = table[1][code];
+
+		code = max_code;
+		if(code < (1<<MaxLzwBits))
+		  {
+			table[0][code] = oldcode;
+			table[1][code] = firstcode;
+			++max_code;
+			if((max_code >= max_code_size) &&
+			   (max_code_size < (1<<MaxLzwBits)))
+			  {
+				max_code_size *= 2;
+				++code_size;
+			  }
+		  }
+
+		oldcode = incode;
+
+		if(sp > stack)
+		  return *--sp;
+
+	  }
+
+	return code;
+	
+  }
+
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Read image data from GIF
+   */
+  // ----------------------------------------------------------------------
+
+  void read_image(FILE * theInput,
+				  NFmiImage & theImage,
+				  unsigned int theWidth,
+				  unsigned int theHeight,
+				  int theTransparent,
+				  const unsigned char theColorMap[3][MaxColors],
+				  bool theInterlaceFlag,
+				  bool theIgnoreFlag)
+  {
+	// Initialize decompression
+
+	int c;
+	if( (c = fgetc(theInput)) == EOF)
+	  throw runtime_error("Image data missing in GIF");
+
+	if(lzwreadbyte(theInput,true,c) < 0)
+	  throw runtime_error("Error read GIF image section");
+
+	// If this is an uninteresting part of the GIF animation, ignore it
+
+	if(theIgnoreFlag)
+	  {
+		while(lzwreadbyte(theInput,false,c) >= 0)
+		  ;
+		return;
+	  }
+
+	unsigned int xpos = 0, ypos = 0, pass = 0;
+	int v;
+	while((v = lzwreadbyte(theInput,false,c)) >= 0)
+	  {
+		if(v == theTransparent)
+		  theImage(xpos,ypos) = NFmiColorTools::TransparentColor;
+		else
+		  theImage(xpos,ypos) = NFmiColorTools::MakeColor(theColorMap[0][v],
+														  theColorMap[1][v],
+														  theColorMap[2][v]);
+		++xpos;
+		if(xpos == theWidth)
+		  {
+			xpos = 0;
+			if(!theInterlaceFlag)
+			  ++ypos;
+			else
+			  {
+				switch(pass)
+				  {
+				  case 0: ypos += 8; break;
+				  case 1: ypos += 8; break;
+				  case 2: ypos += 4; break;
+				  case 3: ypos += 2; break;
+				  }
+				if(ypos >= theHeight)
+				  {
+					++pass;
+					switch(pass)
+					  {
+					  case 1: ypos = 4; break;
+					  case 2: ypos = 2; break;
+					  case 3: ypos = 1; break;
+					  default: goto fini;
+					  }
+				  }
+			  }
+		  }
+		if(ypos >= theHeight)
+		  break;
+	  }
+	
+  fini:
+	
+	// We do not throw since pbmplus merely warns about this
+	if(lzwreadbyte(theInput,false,c) >= 0)
+	  ; // throw runtime_error("Too much data in GIF image section");
+	
+  }
 
   // ----------------------------------------------------------------------
   // Read GIF image
   // ----------------------------------------------------------------------
   
-  void NFmiImage::ReadGIF(FILE *out)
+  void NFmiImage::ReadGIF(FILE *in)
   {
+	// read the first image, if the GIF is an animation
+	const int imagenumber = 1;
+	
+	// General purpose read buffer
+	unsigned char buf[16];
+
+	// Read and verify the signature
+	if(!readok(in,buf,6))
+	  throw runtime_error("Failed to read GIF magic number");
+
+	if(buf[0] != 'G' ||
+	   buf[1] != 'I' ||
+	   buf[2] != 'F' ||
+	   buf[3] != '8' ||
+	   (buf[4] != '7' && buf[4] != '9') ||
+	   buf[5] != 'a')
+	  {
+		throw runtime_error("GIF file being read does not have GIF87a or GIF89a signature");
+	  }
+	
+	// Read the screen descriptor
+	if(!readok(in,buf,7))
+	  throw runtime_error("Failed to read GIF screen descriptor");
+	
+	// Establish the screen info
+	
+	GifScreen screen;
+	screen.width           = LM_to_uint(buf[0],buf[1]);
+	screen.height          = LM_to_uint(buf[2],buf[3]);
+	screen.bitpixel        = 2<<(buf[4]&0x07);
+	screen.colorresolution = (((buf[4]&0x70)>>3)+1);
+	screen.background      = buf[5];
+	screen.aspectratio     = buf[6];
+	
+	// Read global color map if there is one
+
+	if(bitset(buf[4],LocalColorMap))
+	  {
+		read_colormap(in,screen.bitpixel,screen.colormap);
+	  }
+	
+	// We ignore the aspect ratio settings, do not even warn about it
+	// See giftoppm code for more details
+
+	// Allocate the image
+	Allocate(screen.width, screen.height);
+
+	// Now we enter a loop to read the image information
+
+	int imagecount = 0;
+	for(;;)
+	  {
+		int c;
+		if( (c = fgetc(in)) == EOF)
+		  throw runtime_error("GIF file ended abrubtly");
+
+		// GIF terminator
+		if(c == ';')
+		  {
+			if(imagecount < imagenumber)
+			  throw runtime_error("Too few images in the GIF file");
+			return;
+		  }
+
+		// Extension block
+		if(c == '!')
+		  {
+			int label;
+			if( (label = fgetc(in)) == EOF)
+			  throw runtime_error("Read error in GIF extension block");
+			read_extension(in,label,screen);
+			continue;
+		  }
+
+		// Must be a start character then..
+		if(c != ',')
+		  continue; // throw runtime_error("Block start character missing from GIF stream");
+
+		// Now we have an image to read
+		++imagecount;
+
+		if(!readok(in,buf,9))
+		  throw runtime_error("Failed to read GIF frame size");
+
+		const bool use_global_colormap = !bitset(buf[8],LocalColorMap);
+		const int bitpixel =  1<<((buf[8]&0x07)+1);
+
+		const int width  = LM_to_uint(buf[4],buf[5]);
+		const int height = LM_to_uint(buf[6],buf[7]);
+		const bool interlace = bitset(buf[8],Interlace);
+		const bool ignore = (imagecount != imagenumber);
+
+		if(use_global_colormap)
+		  {
+			read_image(in,*this,width,height,screen.transparent,screen.colormap,interlace,ignore);
+		  }
+		else
+		  {
+			unsigned char colormap[3][MaxColors];
+			read_colormap(in,bitpixel,colormap);
+			read_image(in,*this,width,height,screen.transparent,colormap,interlace,ignore);
+		  }
+	  }
+
   }
   
   // ----------------------------------------------------------------------
@@ -90,7 +640,7 @@ namespace Imagine
 	
 	bool ignorealpha = !itsSaveAlphaFlag;
 	
-	bool overflow = AddColors(theColors,kFmiMaxGifColors,opaquethreshold,ignorealpha);
+	bool overflow = AddColors(theColors,MaxColors,opaquethreshold,ignorealpha);
 	
 	// If overflow occurred, we must quantize the image
 	
