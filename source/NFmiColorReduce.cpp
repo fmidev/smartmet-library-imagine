362d277ff4002c833e9f89abecdaa9adb42d912d
mheiskan <mheiskan>
2004-09-06 16:11:50 +0000

    Uusi versio värien vähennys algoritmista on huomattavasti nopeampi, muttei välttämättä laadullisesti parempi, koska populariteettia suositaan nyt enemmän kuin diversiteettiä. Vaatii lisätestausta.

--- source/NFmiColorReduce.cpp
+++ source/NFmiColorReduce.cpp
@@ -23,6 +23,7 @@
 #include "NFmiColorTools.h"
 #include "NFmiNearTree.h"
 
+#include <iostream>
 #include <list>
 #include <map>
 #include <vector>
@@ -76,37 +77,12 @@ namespace Imagine
 
 	}
 
-	// -----------------------------------------------------------------------
-	/*!
-	 * \brief Structure containing color reduction information
-	 */
-	// ----------------------------------------------------------------------
-	
-	const float MAXERROR = 100000000;
-  
-	struct ColorInfo
-	{
-	  NFmiColorTools::Color color;
-	  float ratio;		// percentage 0-1
-
-	  float error;			// error estimate to nearest chosen color
-	  int nearest;		// index of nearest chosen color
-	  
-	  ColorInfo(NFmiColorTools::Color theColor, float theRatio)
-		: color(theColor)
-		, ratio(theRatio)
-		, error(MAXERROR)
-		, nearest(-1)
-	  { }
-
-	}; // struct ColorInfo
-
 	// ----------------------------------------------------------------------
 	/*!
 	 * \brief Initialize the given gamma correction table
 	 */
 	// ----------------------------------------------------------------------
-	
+
 	void init_gamma_table(vector<float> & theTable, bool & theFlag)
 	{
 	  const float gamma = 2.2;
@@ -121,38 +97,243 @@ namespace Imagine
 
 	// ----------------------------------------------------------------------
 	/*!
-	 * \brief Calculate difference between two colors
+	 * \brief A near-tree of colors and their occurrance counts
 	 *
-	 * The code is from http://www.compuphase.com/cmetric.htm with
-	 * an extra term added for alpha difference and more weigth given
-	 * for the red color.
+	 * The idea is to feed in colors in the order of their popularity
+	 * and then use the near-tree maximum distance information
+	 * to group colors for color reduction.
 	 */
 	// ----------------------------------------------------------------------
-	
-	inline
-	float color_error(int r1, int g1, int b1, int a1,
-					  NFmiColorTools::Color color)
-	{
-	  static vector<float> gamma_table(256,0);
-	  static bool initialized = false;
-	  
-	  if(!initialized) init_gamma_table(gamma_table,initialized);
-	  
-	  float r2 = gamma_table[NFmiColorTools::GetRed(color)];
-	  float g2 = gamma_table[NFmiColorTools::GetGreen(color)];
-	  float b2 = gamma_table[NFmiColorTools::GetBlue(color)];
-	  float a2 = gamma_table[NFmiColorTools::GetAlpha(color)];
-	  
-	  float rmean = (gamma_table[r1]+r2)/2;
-	  float r = gamma_table[r1]-r2;
-	  float g = gamma_table[g1]-g2;
-	  float b = gamma_table[b1]-b2;
-	  float a = gamma_table[a1]-a2;
 
-	  return sqrt((((512+rmean)*r*r)/256) + 4*g*g + (((767-rmean)*b*b)/256) + a*a);
+	class ColorTree
+	{
+	public:
+	  typedef NFmiColorTools::Color value_type;
+
+	  ColorTree();
+	  void insert(value_type theColor);
+	  void insert(value_type theColor, float theMaxError);
+
+	  value_type nearest(value_type theColor);
+
+	private:
+
+	  ColorTree(const ColorTree & theTree);
+	  ColorTree & operator=(const ColorTree & theTree);
+
+	  static float distance(value_type theColor1, value_type theColor2);
+	  bool nearest(value_type theColor, value_type & theNearest, float & theRadius) const;
+
+	  auto_ptr<value_type> itsLeftObject;
+	  auto_ptr<value_type> itsRightObject;
+	  float itsMaxLeft;
+	  float itsMaxRight;
+	  auto_ptr<ColorTree> itsLeftBranch;
+	  auto_ptr<ColorTree> itsRightBranch;
+
+	};
+
+	// ----------------------------------------------------------------------
+	/*!
+	 * \brief Default constructor
+	 */
+	// ----------------------------------------------------------------------
+
+	ColorTree::ColorTree()
+	  : itsLeftObject()
+	  , itsRightObject()
+	  , itsMaxLeft(-1.0)
+	  , itsMaxRight(-1.0)
+	  , itsLeftBranch()
+	  , itsRightBranch()
+	{
+	}
+
+	// ----------------------------------------------------------------------
+	/*!
+	 * \brief Euclidian distance between two colors
+	 *
+	 * See http://www.compuphase.com/cmetric.htm
+	 */
+	// ----------------------------------------------------------------------
+
+	float ColorTree::distance(ColorTree::value_type theColor1,
+							  ColorTree::value_type theColor2)
+	{
+
+	  static vector<float> gamma(256,0);
+	  static bool initialized = false;
+
+	  if(!initialized) init_gamma_table(gamma,initialized);
+
+	  const float r = (gamma[NFmiColorTools::GetRed(theColor1)] -
+					   gamma[NFmiColorTools::GetRed(theColor2)]);
+	  const float g = (gamma[NFmiColorTools::GetGreen(theColor1)] -
+					   gamma[NFmiColorTools::GetGreen(theColor2)]);
+	  const float b = (gamma[NFmiColorTools::GetBlue(theColor1)] -
+					   gamma[NFmiColorTools::GetBlue(theColor2)]);
+	  const float a = (NFmiColorTools::GetAlpha(theColor1) -
+					   NFmiColorTools::GetAlpha(theColor2));
+
+	  return sqrt(3.0*r*r+4.0*g*g+2.0*b*b+a*a);
 
 	}
 
+
+	// ----------------------------------------------------------------------
+	/*!
+	 * \brief Insert a color into the color tree
+	 *
+	 * Algorithm:
+	 *
+	 *  -# If left position is free, put the color there
+	 *  -# If right position is free, put the color there
+	 *  -# Insert into subtree whose root is closer to the color
+	 */
+	// ----------------------------------------------------------------------
+
+	void ColorTree::insert(ColorTree::value_type theColor)
+	{
+	  if(itsLeftObject.get() == 0)
+		itsLeftObject.reset(new value_type(theColor));
+
+	  else if(itsRightObject.get() == 0)
+		itsRightObject.reset(new value_type(theColor));
+
+	  else
+		{
+		  const float dist_left = distance(theColor,*itsLeftObject);
+		  const float dist_right = distance(theColor,*itsRightObject);
+
+		  if(dist_left > dist_right)
+			{
+			  if(itsRightBranch.get() == 0)
+				itsRightBranch.reset(new ColorTree);
+
+			  // note that constructor sets itsMaxRight to be negative
+
+			  itsMaxRight = max(itsMaxRight,dist_right);
+
+			  itsRightBranch->insert(theColor);
+			}
+		  else
+			{
+			  if(itsLeftBranch.get() == 0)
+				itsLeftBranch.reset(new ColorTree);
+
+			  // note that constructor sets itsMaxLeft to be negative
+
+			  itsMaxLeft = max(itsMaxLeft,dist_left);
+
+			  itsLeftBranch->insert(theColor);
+
+			}
+		}
+	}
+
+	// ----------------------------------------------------------------------
+	/*!
+	 * \brief Insert into colortree unless there already is a closeby color
+	 *
+	 * \param theColor The color to insert
+	 * \param theMaxError The allowed maximum error
+	 */
+	// ----------------------------------------------------------------------
+
+	void ColorTree::insert(ColorTree::value_type theColor,
+						   float theMaxError)
+	{
+	  // Handle the trivial case quickly
+	  if(theMaxError <= 0)
+		insert(theColor);
+
+	  else
+		{
+		  value_type color = nearest(theColor);
+		  if(distance(color,theColor) >= theMaxError)
+			insert(theColor);
+		}
+	}
+
+	// ----------------------------------------------------------------------
+	/*!
+	 * \brief Find the nearest color
+	 *
+	 * \param theColor The color for which to find the nearest color
+	 * \return The nearest color
+	 */
+	// ----------------------------------------------------------------------
+
+	ColorTree::value_type ColorTree::nearest(ColorTree::value_type theColor)
+	{
+	  value_type bestcolor;
+	  float radius = -1;
+	  if(!nearest(theColor,bestcolor,radius))
+		throw runtime_error("Invalid use of color reduction tables");
+	  return bestcolor;
+	}
+	
+	// ----------------------------------------------------------------------
+	/*!
+	 * \brief Recursively find nearest color
+	 *
+	 * This is only intended to be used by the public nearest method
+	 */
+	// ----------------------------------------------------------------------
+
+	bool ColorTree::nearest(ColorTree::value_type theColor,
+							ColorTree::value_type & theNearest,
+							float & theRadius) const
+	{
+	  double tmpradius;
+	  bool found = false;
+
+	  // first test each of the left and right positions to see if
+	  // one holds a color nearer than the nearest so far discovered
+
+	  if(itsLeftObject.get() != 0)
+		{
+		  tmpradius = distance(theColor,*itsLeftObject);
+		  if(theRadius<0  || tmpradius<=theRadius)
+			{
+			  theRadius = tmpradius;
+			  theNearest = *itsLeftObject;
+			  found = true;
+			}
+		}
+
+	  if(itsRightObject.get() != 0)
+		{
+		  tmpradius = distance(theColor,*itsRightObject);
+		  if(theRadius<0  || tmpradius<=theRadius)
+			{
+			  theRadius = tmpradius;
+			  theNearest = *itsRightObject;
+			  found = true;
+			}
+		}
+
+	  // if theRadius is negative at this point, the tree is empty
+	  // on the other hand, if the radius is zero, we found a match
+
+	  if(theRadius <= 0)
+		return found;
+
+	  // Now we test to see if the branches below might hold an object
+	  // nearer than the best so far found. The triangle rule is used
+	  // to test whether it's even necessary to descend
+
+	  if((itsLeftBranch.get() != 0) &&
+		 ((theRadius + itsMaxLeft) >= distance(theColor,*itsLeftObject)))
+		found |= itsLeftBranch->nearest(theColor,theNearest,theRadius);
+
+	  if((itsRightBranch.get() != 0) &&
+		 ((theRadius + itsMaxRight) >= distance(theColor,*itsRightObject)))
+		found |= itsRightBranch->nearest(theColor,theNearest,theRadius);
+
+	  return found;
+	  
+	}
 
   } // namespace anonymous
 
@@ -196,37 +377,33 @@ namespace Imagine
 	 *
 	 * Algorithm:
 	 *
-	 *   -# Calculate the occurrance count of each color
-	 *   -# Choose colors one at a time until error is small enough
+	 *  -# Calculate the histogram
+	 *  -# In order of popularity, feed the colors into a color tree
+	 *     which discards colors which are close enough to a color
+	 *     chosen earlier.
+	 *  -# For each color, ask the color tree which is the nearest
+	 *     color for it.
+	 *  -# Replace the original colors with the colors found in the previous
+	 *     step.
 	 *
-	 * The original selection algorithm used in gifsicle and xv is as follows:
+	 * Note that this is very very close to the common popularity based
+	 * algorithm. However, the twist is that we have an adaptive error
+	 * criteria, we are not aiming for a fixed number of colors. Hence
+	 * we get the benefit of the speed of the popularity based algorithm
+	 * but will never get too big errors.
 	 *
-	 *   -# Choose color 1 to be the most popular color
-	 *   -# Choose next most popular colors until the associated
-	 *      percentage drops below 1%
-	 *   -# Choose at alternate turns either the most diverse color or the next
-	 *      most popular color
+	 * An algorithm which might produce better quality results would
+	 * be to aim for color diversity as follows:
 	 *
-	 * Both xv and gifsicle terminate when a preset number of colors have
-	 * been chosen. However, we prefer the limit to be adaptive based
-	 * on actual error of the chosen color.
+	 *  -# Perform the insertion phase as in algorithm 1, but
+	 *     with the error limit doubled.
+	 *  -# Perform the insertion phase as in algorithm 1, now
+	 *     with a normal error limit
+	 *  -# Continue by asking the nearest color for all colors
 	 *
-	 * Our algorithm is:
-	 *
-	 *  -# Find the most diverse color
-	 *  -# From a given radius from the diverse color, choose the most
-	 *     popular one
-	 *
-	 * Also, we use the color distance formula given at
-	 * http://www.compuphase.com/cmetric.htm
-	 * to improve finding the diverse color.
-	 *
-	 * The first step is essential for preserving the color of smallish
-	 * patches of color, since any changes in their color in animation
-	 * are noticeable. Changes in isolated pixels are much less noticeable.
-	 *
-	 * The radius may or may not match the desired maximum error used in the
-	 * termination criteria.
+	 * However, it is not clear that doubling the error in phase 1
+	 * would be the ideal approach. The ideal multiplier may be
+	 * less than or more than 2.
 	 *
 	 * \param theImage The image to modify
 	 * \param theMaxError The allowed maximum error
@@ -245,169 +422,53 @@ namespace Imagine
 	  // Calculate the histogram
 	  
 	  Histogram histogram = CalcHistogram(theImage);
-	  
-	  // Start choosing
-	  
-	  typedef vector<ColorInfo> ChosenColors;
-	  typedef list<ColorInfo> ColorList;
-	  
-	  ChosenColors chosen_colors;
-	  ColorList todo_colors;
-	  ColorList close_colors;
-	  
-	  // Populate the todo-colors
-	  
-	  const float npixels = static_cast<float>(theImage.Width()*theImage.Height());
-	  
-	  for(Histogram::const_iterator it = histogram.begin();
-		  it != histogram.end();
-		  ++it)
-		{
-		  todo_colors.push_back(ColorInfo(it->second,it->first/npixels));
-		}
-	  
-	  // Choose the first color
-	  
-	  ColorInfo color = todo_colors.front();
-	  color.nearest = 0;
-	  chosen_colors.push_back(color);
-	  todo_colors.pop_front();
-	  
-	  while(!todo_colors.empty())
-		{
-		  int red   = GetRed(color.color);
-		  int green = GetGreen(color.color);
-		  int blue  = GetBlue(color.color);
-		  int alpha = GetAlpha(color.color);
-		  
-		  ColorList::iterator chosen = todo_colors.end();
-		  float maxerror = 0;		// maximum true error remaining
-		  
-		  // Recalculate the errors, and simultaneously find
-		  // the most errorneous color
-		  
-		  for(ColorList::iterator it = todo_colors.begin();
-			  it != todo_colors.end();
-			  )
-			{
-			  // Error compared to chosen color
-			  
-			  const float err = color_error(red,green,blue,alpha,it->color);
-			  
-			  // If it is smaller than the previously calculated error,
-			  // update the information on the nearest color
-			  
-			  if(err < it->error)
-				{
-				  it->error = err;
-				  it->nearest = chosen_colors.size()-1;
 
-				  // Move the color away from the todo-list if the
-				  // remaining error is small
+	  // Build a pruned tree of colors. All colors popular enough
+	  // (more than 1%) are forced in, others have an error criterion
 
-				  if(err < theMaxError)
-					{
-					  close_colors.push_back(*it);
-					  ColorList::iterator tmp = it;
-					  ++it;
-					  todo_colors.erase(tmp);
-					}
-				  else
-					{
-					  if(it->error > maxerror)
-						{
-						  chosen = it;
-						  maxerror = it->error;
-						}
-					  ++it;
-					}
-				}
-			  else
-				++it;
+	  ColorTree tree;
 
-			}
+	  {
+		const float limit = 0.01*theImage.Width()*theImage.Height();
 
-		  // If the next todo-color is popular enough, choose it
-		  // no matter what
-		  
-		  if(todo_colors.front().ratio > 0.01)
-			{
-			  chosen = todo_colors.begin();
-			}
-		  else
-			{
-			  // Now, if the maximum error is small enough, we're done
-			  
-			  if(maxerror < theMaxError)
-				break;
-			  
-			  // Otherwise find the most popular color in the neighbourhood
-			  // of the most errorneous color. However, we ignore any colors
-			  // which are closer to a selected color than to the diverse color.
-			  // This aids in getting diverse colors representation in the colormap
-			  
-			  ColorList::iterator best = chosen;
-			  red   = GetRed(chosen->color);
-			  green = GetGreen(chosen->color);
-			  blue  = GetBlue(chosen->color);
-			  alpha = GetAlpha(chosen->color);
-			  
-			  for(ColorList::iterator jt = todo_colors.begin();
-				  jt != todo_colors.end();
-				  ++jt)
-				{
-				  float err = color_error(red,green,blue,alpha,jt->color);
-				  
-				  if(err < theMaxError &&
-					 jt->ratio > best->ratio &&
-					 jt->error > 0.5*err) // jt->error > 0.5*theMaxError)
-					{
-					  best = jt;
-					}
-				}
-			  
-			  // Choose the best one
-			  
-			  chosen = best;
-			}
-		  
-		  color = *chosen;
-		  color.nearest = chosen_colors.size();
-		  chosen_colors.push_back(color);
-		  todo_colors.erase(chosen);
+		for(Histogram::const_iterator it = histogram.begin();
+			it != histogram.end();
+			++it)
+		  {
+			if(it->first >= limit)
+			  tree.insert(it->second);
+			else
+			  tree.insert(it->second,2*theMaxError);
+		  }
+	  }
 
-		}
-	  
-	  // Now fix the colors in the image. To help us we build a map
-	  // with colors as key and the respective chosen color index
-	  // as the value
-	  
-	  typedef map<Color,int> ColorMap;
+	  {
+		for(Histogram::const_iterator it = histogram.begin();
+			it != histogram.end();
+			++it)
+		  {
+			tree.insert(it->second,theMaxError);
+		  }
+	  }
+
+	  // Then build the colormap transformation map
+
+	  typedef map<Color,Color> ColorMap;
 	  ColorMap colormap;
-	  
-	  for(ChosenColors::const_iterator jt = chosen_colors.begin();
-		  jt != chosen_colors.end();
-		  ++jt)
-		{
-		  colormap.insert(ColorMap::value_type(jt->color,jt->nearest));
-		}
 
-	  for(ColorList::const_iterator lt = close_colors.begin();
-		  lt != close_colors.end();
-		  ++lt)
-		{
-		  colormap.insert(ColorMap::value_type(lt->color,lt->nearest));
-		}
-	  
-	  for(ColorList::const_iterator kt = todo_colors.begin();
-		  kt != todo_colors.end();
-		  ++kt)
-		{
-		  colormap.insert(ColorMap::value_type(kt->color,kt->nearest));
-		}
-	  
-	  // Now for each image pixel, find the associated colormap index
-	  // and replace the original color with chosen color
+	  {
+		for(Histogram::const_iterator it = histogram.begin();
+			it != histogram.end();
+			++it)
+		  {
+			Color color = tree.nearest(it->second);
+			colormap.insert(ColorMap::value_type(it->second,color));
+		  }
+	  }
+
+	  // Now perform the replacements. Since we expect to encounter
+	  // sequences of color, we speed of the searches by caching
+	  // the last replacement.
 
 	  NFmiColorTools::Color last_color = NFmiColorTools::NoColor;
 	  NFmiColorTools::Color last_choice = NFmiColorTools::NoColor;
@@ -417,15 +478,15 @@ namespace Imagine
 		  {
 			if(theImage(i,j) != last_color)
 			  {
-				ColorMap::const_iterator cit = colormap.find(theImage(i,j));
-				if(cit == colormap.end())
-				  throw runtime_error("Internal error in color reduction, failed to find color");
 				last_color = theImage(i,j);
-				last_choice = chosen_colors[cit->second].color;
+
+				ColorMap::const_iterator it = colormap.find(last_color);
+				if(it == colormap.end())
+				  throw runtime_error("Internal error in color reduction, failed to find color");
+				last_choice = it->second;
 			  }
 			theImage(i,j) = last_choice;
 		  }
-	  
 	}
 	
   } // namespace NFmiColorReduce
