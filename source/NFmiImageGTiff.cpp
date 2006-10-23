55dcd9b854654a371dd104d32081c62e937bcf95
toivonem <toivonem>
2006-10-23 13:20:52 +0000

    Mirroroidaan tiff-rasterin vertical scanlinet. Jostain syystä 0,0 asetetaan vas. alakulmaan.

--- source/NFmiImageGTiff.cpp
+++ source/NFmiImageGTiff.cpp
@@ -35,8 +35,24 @@ namespace Imagine
     raster = static_cast<uint32*>(_TIFFmalloc(npixels * sizeof (uint32)));
     if (raster != NULL) {
        if (TIFFReadRGBAImage(in, w, h, raster, 0)) {
+		  // mirroroidaan scanlinet, sillä TIFFReadRGBAImage asettaa 0,0:n vasempaan alakulmaan 
+
+		  uint32 *wrk_line = static_cast<uint32*>(_TIFFmalloc(w * sizeof (uint32)));
+		  for( uint32 row = 0; row < h / 2; row++ ) {
+			uint32	*top_line, *bottom_line;
+
+			top_line = raster + w * row;
+			bottom_line = raster + w * (h-row-1);
+
+			_TIFFmemcpy(wrk_line, top_line, 4*w);
+			_TIFFmemcpy(top_line, bottom_line, 4*w);
+			_TIFFmemcpy(bottom_line, wrk_line, 4*w);
+		  }
+		  _TIFFfree( wrk_line );
+
  		  int width = static_cast<int>(w);
  		  int height = static_cast<int>(h);
+
 		  Allocate(width,height);
 		  for(int j=0; j<height; j++) 
 			for(int i=0; i<width; i++) {
@@ -46,7 +62,7 @@ namespace Imagine
 			  const int b = color & 0x00FF0000;
 			  const int g = color & 0x0000FF00;
 			  const int r = color & 0x000000FF;
-			  (*this)(i,j) = NFmiColorTools::MakeColor(a,r,g,b);
+			  (*this)(i,j) = NFmiColorTools::MakeColor(r,g,b,a);
 			}
         }
         _TIFFfree(raster);
