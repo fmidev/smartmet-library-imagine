e905686aa3b7e9e6d82315acf3e33b2ab3f6100f
Mika Heiskanen <mika.heiskanen@fmi.fi>
2013-09-27 11:01:18 +0300

    Added a function for filtering an ESRI shape

---
+++ include/NFmiEsriTools.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "NFmiEsriShape.h"
+#include <string>
+
+namespace Imagine
+{
+  namespace NFmiEsriTools
+  {
+	NFmiEsriShape * filter(const NFmiEsriShape & theShape,
+						   const std::string & theCondition);
+  }
+}
